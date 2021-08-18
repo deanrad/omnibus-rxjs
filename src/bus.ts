1bd7dd0e9d644a5b22abbfa4d660c74c8ee2ee6a
Dean Radcliffe <deanmisc@yahoo.com>
2021-08-18 13:31:25 -0500

    WIP Typedoc

--- src/bus.ts
+++ src/bus.ts
@@ -3,10 +3,32 @@ import { filter } from 'rxjs/operators';
 export type Predicate<T> = (item: T) => boolean;
 export type ResultCreator<T, U> = (item: T) => Observable<U>;
 
-type TapObserver<T> = PartialObserver<T> | {
-  start: (item:T) => void;
+type TapObserver<T> =
+  | PartialObserver<T>
+  | {
+      start: (item: T) => void;
+    };
+
+interface EventBus<T> {
+  query(matcher: Predicate<T>): Observable<T>;
+  trigger(item: T): void;
+  listen<U>(
+    matcher: Predicate<T>,
+    handler: ResultCreator<T, U>,
+    observer?: TapObserver<U>
+  ): Subscription;
 }
-export class Omnibus<T> {
+/**
+ * An instance of an Omnibus implements the EventBus interface,
+ * providing type-safe ways of triggering, filtering, and running
+ * side-effects in response to listening to events.
+ *
+ * In addition, an Omnibus instance allows delcarative concurrency
+ * control, and provides ample means to dispose of resources at the
+ * callers' control.
+ * @see EventBus
+ */
+export class Omnibus<T> implements EventBus<T>{
   private channel: Subject<T>;
 
   constructor() {
@@ -35,10 +57,10 @@ export class Omnibus<T> {
    * as is needed when creating Actions out of payloads.
    * @param item the payload
    * @param mapper the function, usually an actionCreator
-   * @returns 
+   * @returns
    */
-  public triggerMap<X>(item: X, mapper: (i:X)=>T) {
-    this.channel.next(mapper(item))
+  public triggerMap<X>(item: X, mapper: (i: X) => T) {
+    this.channel.next(mapper(item));
   }
 
   /**
@@ -47,9 +69,9 @@ export class Omnibus<T> {
    * @param handler Creates an Observable of work. Called for each matching event. ConcurrencyMode is applied to it.
    * @returns a subscription that can be used to unsubscribe the listener, thereby canceling work in progress.
    */
-  public listen<T, U>(
-    matcher: Predicate<T>,
-    handler: ResultCreator<T, U>,
+  public listen<SubType extends T, U>(
+    matcher: Predicate<SubType>,
+    handler: ResultCreator<SubType, U>,
     observer?: TapObserver<U>
   ) {
     //
