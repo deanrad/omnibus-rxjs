085ce8525b8c5d955fa82784975ca9a906f1f97c
Dean Radcliffe <dean.radcliffe@rallyhealth.com>
2021-08-22 22:20:03 -0500

    WIP parsing

--- scripts/code-stats.js
+++ scripts/code-stats.js
@@ -1,25 +1,42 @@
 const esprima = require('esprima');
 const esquery = require('esquery');
+const tsquery = require('@phenomnomnominal/tsquery').tsquery;
 
+const fs = require('fs');
 // for each interesting file
-const dir = 'dist/tsc/src';
-const files = ['bus'];
+const files = [
+  'dist/tsc/src/bus.js',
+  'src/bus.ts',
+  'dist/tsc/src/utils.js',
+  'src/utils.ts',
+];
+
 const astNodeCount = {};
 files.forEach((f) => {
-  // LEFTOFF!
+  const nodeCount = getNodeCount(f);
+  astNodeCount[f] = nodeCount;
 });
-// var ast = esprima.parse(str);
-// selectorAst = esquery.parse('*');
-// matches = esquery.match(ast, selectorAst);
-// nodeCount = matches.length;
 
-// write to stdout
-const mockOut = {
-  js: {
-    astNodeCount: {
-      'bus.js': 66,
-    },
-  },
-};
+function getNodeCount(f) {
+  const count = f.endsWith('.js') ? getJSNodeCount(f) : getTSNodeCount(f);
+  return count;
+}
 
-process.stdout.write(JSON.stringify(mockOut, null, 2) + '\n', 'utf-8');
+function getJSNodeCount(filename) {
+  const contents = fs.readFileSync(filename, 'utf-8');
+  const selector = esquery.parse('*');
+  const ast = esprima.parse(contents, { sourceType: 'module' });
+  const matches = esquery.match(ast, selector);
+  return matches.length;
+}
+
+function getTSNodeCount(filename) {
+  const contents = fs.readFileSync(filename, 'utf-8');
+  const ast = tsquery.ast(contents);
+  const nodes = tsquery(ast, '*');
+  return nodes.length;
+}
+
+const nodeCounts = { astNodeCount };
+
+process.stdout.write(JSON.stringify(nodeCounts, null, 2) + '\n', 'utf-8');
