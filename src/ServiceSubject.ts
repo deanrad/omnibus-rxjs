1c58803f04c0c47e776b9f9d4e132319b8cad3ce
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2022-07-01 09:32:31 -0500

    WIP

---
+++ src/ServiceSubject.ts
@@ -0,0 +1,55 @@
+import { Omnibus } from '../src/bus';
+import { createService, Service, ActionCreators } from '../src/createService';
+import { distinctUntilChanged, Observable } from 'rxjs';
+import { Action, ActionCreator } from 'typescript-fsa';
+
+export class ServiceSubject<TRequest, TNext, TError, TState> {
+  private service: Service<TRequest, TNext, TError, TState>;
+  public bus: Omnibus<Action<any>>;
+  public actions: ActionCreators<TRequest, TNext, TError>;
+  public state: Observable<TState>;
+
+  constructor(
+    namespace: string,
+    handler: (e: TRequest) => any,
+    reducerFactory: (
+      acs?: ActionCreators<TRequest, TNext, TError>
+    ) => (state: TState, action: Action<any>) => TState
+  ) {
+    this.bus = new Omnibus();
+    this.service = createService<TRequest, TNext, TError, TState>(
+      namespace,
+      this.bus,
+      handler,
+      reducerFactory
+    );
+    this.actions = this.service.actions;
+    this.state = this.service.state.pipe(distinctUntilChanged());
+  }
+
+  public next(request: TRequest) {
+    this.service(request);
+  }
+  public listen(
+    matcher: Parameters<Omnibus<Action<any>>['listen']>[0] | ActionCreator<any>,
+    ...args: [any, any?, any?]
+  ) {
+    return this.bus.listen(this.toPredicate(matcher), ...args);
+  }
+  public guard(
+    matcher: Parameters<Omnibus<Action<any>>['listen']>[0] | ActionCreator<any>,
+    ...args: [any]
+  ) {
+    return this.bus.guard(this.toPredicate(matcher), ...args);
+  }
+
+  private toPredicate(
+    matcher: Parameters<Omnibus<Action<any>>['listen']>[0] | ActionCreator<any>
+  ) {
+    return (
+      (matcher as ActionCreator<TRequest>).match
+        ? (matcher as ActionCreator<TRequest>).match
+        : matcher
+    ) as (i: Action<any>) => i is Action<any>;
+  }
+}
--- test/3rd-party.spec.ts
+++ test/3rd-party.spec.ts
@@ -1,4 +1,4 @@
-import { of, asapScheduler, asyncScheduler } from 'rxjs';
+import { of, asapScheduler, asyncScheduler, Subject } from 'rxjs';
 import * as ExampleService from '../example/debounced-search/searchService';
 
 /** Specs that document how 3rd party libs we may use interact */
@@ -6,15 +6,15 @@ describe('Reference: FSA', () => {
   it('creates an action', () => {
     const action = ExampleService.searchRequestCreator({ query: 'app' });
     expect([action]).toMatchInlineSnapshot(`
-  Array [
-    Object {
-      "payload": Object {
-        "query": "app",
-      },
-      "type": "search/request",
-    },
-  ]
-  `);
+        Array [
+          Object {
+            "payload": Object {
+              "query": "app",
+            },
+            "type": "search/request",
+          },
+        ]
+      `);
   });
   it('provides a predicate #match', () => {
     expect(ExampleService.searchRequestCreator.match).toMatchInlineSnapshot(
@@ -61,6 +61,110 @@ describe('Reference: RxJS', () => {
       expect(result).toBeUndefined();
       await new Promise((resolve) => setTimeout(resolve, 0));
       expect(result).toEqual(2);
+    });
+  });
+
+  describe('Subjects', () => {
+    describe('Errors', () => {
+      it('subject.complete() stops listeners ', () => {
+        const s = new Subject<number>();
+        const seen = [] as number[];
+        const listener1 = s.asObservable().subscribe({
+          next(v) {
+            seen.push(v);
+          },
+        });
+        expect(listener1.closed).toBeFalsy();
+
+        s.next(1);
+        expect(seen).toEqual([1]);
+
+        s.complete();
+        s.next(2);
+        expect(seen).toEqual([1]);
+        expect(listener1.closed).toBeTruthy();
+
+        // closed is false even after await Promise.resolve();
+        expect(s).toMatchInlineSnapshot(`
+          Subject {
+            "closed": false,
+            "currentObservers": null,
+            "hasError": false,
+            "isStopped": true,
+            "observers": Array [],
+            "thrownError": null,
+          }
+        `);
+      });
+      it('subject.error() stops listeners', () => {
+        const s = new Subject<number>();
+        const seen = [] as number[];
+        const seenErrors = [] as string[];
+        const listener1 = s.asObservable().subscribe({
+          next(v) {
+            seen.push(v);
+          },
+          // must rescue or breaks jest process
+          error(e) {
+            seenErrors.push(e);
+          },
+        });
+        expect(listener1.closed).toBeFalsy();
+
+        s.next(1);
+        expect(seen).toEqual([1]);
+
+        s.error('Oops');
+        s.next(2);
+        expect(seen).toEqual([1]);
+        expect(listener1.closed).toBeTruthy();
+        expect(seenErrors).toMatchInlineSnapshot(`
+          Array [
+            "Oops",
+          ]
+        `);
+
+        // closed is false even after await Promise.resolve();
+        expect(s).toMatchInlineSnapshot(`
+          Subject {
+            "closed": false,
+            "currentObservers": null,
+            "hasError": true,
+            "isStopped": true,
+            "observers": Array [],
+            "thrownError": "Oops",
+          }
+        `);
+
+        // s.closed happens only on unsubscribe (not useful to do)
+        s.unsubscribe();
+        expect(s.closed).toBeTruthy();
+      });
+      it.skip('kill the host process if a sync error not caught', () => {
+        const s = new Subject<number>();
+        const seen = [] as number[];
+        const badSync = s.asObservable().subscribe({
+          next(v) {
+            throw new Error();
+          },
+        });
+        s.next(-1); // kills the process
+      });
+      it.skip('creates unhandled reject  the host process if a sync error not caught', () => {
+        const s = new Subject<number>();
+        const seen = [] as number[];
+        const badSync = s.asObservable().subscribe({
+          next(v) {
+            Promise.resolve().then(() => {
+              throw new Error('async oops');
+            });
+          },
+        });
+        // Results in
+        // [UnhandledPromiseRejection: This error originated either by throwing inside of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). The promise rejected with the reason "Error: async oops".] {
+        //   code: 'ERR_UNHANDLED_REJECTION'
+        s.next(-1);
+      });
     });
   });
 });
