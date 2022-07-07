1822bd94d87d9251afb67b6a30193fa8f249e42a
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2022-07-07 10:44:41 -0500

    1.2.4 Actually export ServiceSubject

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export * from './bus';
 export * from './after';
 export * from './createService';
+export * from './ServiceSubject';
 export * from './toggleMap';
 export { THRESHOLD, observableFromPromisedArray } from './utils';
 export { concat, Observable } from 'rxjs';
