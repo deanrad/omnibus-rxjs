aaf161d540d90efdbdc1ea070103ca3bc24c3a50
Dean Radcliffe <24406+deanrad@users.noreply.github.com>
2022-09-16 12:12:27 -0500

    WIP

--- example/concurrency-viz/app.tsx
+++ example/concurrency-viz/app.tsx
@@ -5,15 +5,23 @@ import {
   exampleState,
   initialState,
 } from './services/blockService';
+import { after } from '../../src/after';
 import { Viz } from './components/Viz';
+import { concat } from 'rxjs';
 
 const viz = document.getElementById('viz') as HTMLElement;
 const reqBtn = document.getElementById('request') as HTMLButtonElement;
+const demoBtn = document.getElementById('demo') as HTMLButtonElement;
 const vizRoot = createRoot(viz);
 
 window.addEventListener('DOMContentLoaded', () => {
   let i = 0;
   reqBtn.addEventListener('click', () => blockService(i++));
-
+  demoBtn.addEventListener('click', () => {
+    concat(
+      after(0, () => blockService(i++)),
+      after(1200, () => blockService(i++))
+    ).subscribe();
+  });
   vizRoot.render(createElement(Viz, { blocks: initialState.blocks }));
 });
